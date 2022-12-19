e9e89ae9dfea670b9d97c19c4f1fe2deeedfbbe9
studyshubham <studyshubham007@gmail.com>
2022-12-19 13:39:08 +0530

    NewCakeContainer Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react'
 import './App.css';
 import { Provider } from 'react-redux'
 import store from './redux/store';
 import CakeContainer from './component/CakeContainer';
 import HooksCakeContainer from './component/HooksCakeContainer';
 import IceCreamContainer from './component/IceCreamContainer';
+import NewCakeContainer from './component/NewCakeContainer';
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
         <HooksCakeContainer />
         <CakeContainer />
         <IceCreamContainer />
+        <NewCakeContainer />
       </div>
     </Provider>
   );
