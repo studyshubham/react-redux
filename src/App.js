d46e02c90c8903c22bc39681c496452a5ab24bb9
studyshubham <studyshubham007@gmail.com>
2022-12-20 21:01:46 +0530

    Added FetchUsers component

--- src/App.js
+++ src/App.js
@@ -6,15 +6,22 @@ import CakeContainer from './component/CakeContainer';
 import HooksCakeContainer from './component/HooksCakeContainer';
 import IceCreamContainer from './component/IceCreamContainer';
 import NewCakeContainer from './component/NewCakeContainer';
+import ItemContainer from './component/ItemContainer';
+import UserContainer from './component/UserContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <UserContainer />
+        {/*
+        <ItemContainer cake />
+        <ItemContainer />
         <HooksCakeContainer />
         <CakeContainer />
         <IceCreamContainer />
         <NewCakeContainer />
+        */}
       </div>
     </Provider>
   );
